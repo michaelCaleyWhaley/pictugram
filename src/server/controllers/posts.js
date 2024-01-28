74788bdd9ccdbc2e71af2be1c3ee02c299c8d456
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2024-01-28 17:10:19 +0000

    update to use async await

--- src/server/controllers/posts.js
+++ src/server/controllers/posts.js
@@ -5,8 +5,9 @@ const Post = require('../models/post');
 const User = require('../models/user');
 
 // get all post
-router.get('/', auth, (req, res) => {
-  Post.find()
+router.get('/', auth, async (_req, res) => {
+  await Post.find()
+    .exec()
     .sort({ date: -1 })
     .then((posts) => res.json(posts))
     .catch((err) => res.status(400).json(`error: ${err}`));
@@ -16,7 +17,7 @@ router.get('/', auth, (req, res) => {
 router.route('/add/:id').post(auth, async (req, res) => {
   const { caption, image } = req.body;
   const { id } = req.params;
-  const user = await User.findById(id);
+  const user = await User.findById(id).exec();
   const newPost = new Post({
     caption,
     image,
@@ -32,36 +33,44 @@ router.route('/add/:id').post(auth, async (req, res) => {
 router.route('/add-comment/:id/:userId').post(auth, async (req, res) => {
   const { id, userId } = req.params;
   const { content } = req.body;
-  const user = await User.findById(userId);
+  const user = await User.findById(userId).exec();
 
   const newContent = new Comment({
     content,
     username: user.username,
   });
   newContent.save().then(() => res.json('Comment Added'));
-  Post.findByIdAndUpdate(
+  const data = await Post.findByIdAndUpdate(
     { _id: id },
     { $push: { comments: newContent } },
-    (err, data) => {
-      if (err) res.status(400).json(`error: ${err}`);
-      else res.status(200).json(data);
-    },
-  );
+  )
+    .exec()
+    .catch((err) => {
+      res.status(400).json(`error: ${err}`);
+    });
+
+  res.status(200).json(data);
 });
 
 // get a post
-router.route('/:id').get(auth, (req, res) => {
-  Post.findById(req.params.id, (err, post) => {
-    if (err) res.status(400).json(`error: ${err}`);
-    else res.status(200).json(post);
-  });
+router.route('/:id').get(auth, async (req, res) => {
+  const post = await Post.findById(req.params.id)
+    .exec()
+    .catch((err) => {
+      res.status(400).json(`error: ${err}`);
+    });
+
+  res.status(200).json(post);
 });
 
 // get all comments for a post
-router.route('/comments/:id').get(auth, (req, res) => {
-  Post.findById(req.params.id, (err, post) => {
-    if (err) res.status(400).json(`error: ${err}`);
-    else res.status(200).json(post.comments);
-  });
+router.route('/comments/:id').get(auth, async (req, res) => {
+  const post = await Post.findById(req.params.id)
+    .exec()
+    .catch((err) => {
+      res.status(400).json(`error: ${err}`);
+    });
+
+  res.status(200).json(post.comments);
 });
 module.exports = router;
