ebbf4f6f6153a01f58e76ab5e973748d3248d9c3
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2024-01-28 18:36:46 +0000

    render styles and page

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Login } from './pages/Login';
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <h1>Hello</h1>
+      <Login />
+    </>
+  );
 }
 
 const domNode = document.getElementById('root');
--- src/server/view/index.js
+++ src/server/view/index.js
@@ -6,7 +6,7 @@ router.route('/').get((req, res) => {
   res
     .status(200)
     .send(
-      '<head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="icon" href="/favicon-48x48.png"></head><div id="root"></div><script defer type="text/javascript" src="/client-bundle.js"></script>',
+      '<head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="icon" href="/favicon-48x48.png"><link rel="stylesheet" href="client-bundle.css"></head><div id="root"></div><script defer type="text/javascript" src="/client-bundle.js"></script>',
     );
 });
 
